bac2700812b61d54e10af13538c071b3f0a5b050
adrianchinghc <achc92@gmail.com>
2016-03-26 18:42:41 +0800

    Fix url problem

--- config.rb
+++ config.rb
@@ -20,7 +20,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "{title}"
+  blog.permalink = "{title}.html"
   # # Matcher for blog source files
   blog.sources = "posts/{year}-{month}-{day}-{title}.html"
   blog.taglink = "tags/{tag}"
--- source/posts/2015-10-20-example-article.html.markdown
+++ source/posts/2015-10-20-example-article.html.markdown
@@ -3,7 +3,7 @@ title: Example Article
 date: 2015-10-20
 tags: example, another
 preferred_url: example-article
-image_url: assets/images/blog/images/post1.png
+image_url: assets/images/blog/post1.png
 ---
 
 This is an example article. You probably want to delete it and write your own articles!
