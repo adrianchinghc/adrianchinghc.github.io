eb5b584c011a8459eaa5fed36c76bf44ea660ea8
adrianchinghc <achc92@gmail.com>
2016-04-04 17:04:04 +0800

    Remove extra js include tag

--- source/assets/javascripts/menu.js
+++ source/assets/javascripts/menu.js
@@ -1,11 +1,12 @@
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('.body').toggleClass('body_hidden')
-   $('#navigation').toggleClass('open');
-  });
-  
-$('.menu').click(function() {
-   $('.body').removeClass('body_hidden')
-   $( "#toggle" ).removeClass( "active" )
-   $('#navigation').removeClass('open');
-  });
+$(document).ready(function() {
+  $('#toggle').click(function() {
+     $(this).toggleClass('active');
+     $('.body').toggleClass('body_hidden')
+     $('#navigation').toggleClass('open');
+    });
+  $('.menu').click(function() {
+     $('.body').removeClass('body_hidden')
+     $( "#toggle" ).removeClass( "active" )
+     $('#navigation').removeClass('open');
+    });
+});
